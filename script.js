60867d39199c7a6d155aa2da2bd1e77654226d31
amancantgit <amankr6660@gmail.com>
2024-12-07 14:15:05 +0530

    UI

---
+++ script.js
@@ -0,0 +1,26 @@
+const box = document.getElementById('box');
+
+// function to move the box randomly within the viewport
+box.addEventListener('mouseover', function(){
+  const viewportWidth = window.innerWidth;
+  const viewportHeight = window.innerHeight;
+  const boxWidth = box.offsetWidth;
+  const boxHeight = box.offsetHeight;
+  const maxX = viewportWidth - box.offsetWidth;
+  const maxY = viewportHeight - box.offsetHeight;
+
+  const newX = Math.floor(Math.random() * maxX);
+  const newY = Math.floor(Math.random() * maxY);
+  box.style.left = newX + 'px';
+  box.style.top = newY + 'px';
+})
+
+box.addEventListener('mouseover', (()=> {
+  box.textContent = `Out of Reach!`
+}))
+
+box.addEventListener('mouseout', (()=> {
+  setTimeout(()=>{
+    box.innerHTML = 'Chase <br>the<br> Shadows'
+  }, 1000)
+}))
